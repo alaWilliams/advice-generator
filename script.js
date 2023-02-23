142834048869669507a1afa903a94487ed70924c
Ala Williams <alicja.m.williams@gmail.com>
2023-02-23 13:20:40 +0100

    js functions and some css work

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const url = 'https://api.adviceslip.com/advice';
 const header = document.querySelector('.header');
+const button = document.querySelector('.button');
+const adviceContent = document.querySelector('.advice-content');
 
-header.innerText = 'work';
+
+const getAdvice = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showAdvice(data))
+    .catch((error) => console.log(error))
+  };
+  
+  button.addEventListener('click', getAdvice);
+
+  const showAdvice = (data) => {
+    header.innerText = `ADVICE #${data.slip.id}`;
+    adviceContent.innerText = `"${data.slip.advice}"`;
+  }
